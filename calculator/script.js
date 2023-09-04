224157b5e2c7f127749c81bfe005fdb7c1edebc0
Santiago Nieto <santiagonm2002@gmail.com>
2023-09-04 16:05:19 -0500

    calculadora a conciencia

--- calculator/script.js
+++ calculator/script.js
@@ -1,34 +1,38 @@
-const calculator = document.querySelector('.calculator');
-const firstInput = calculator.querySelector('input[type="number"]:nth-of-type(1)');
-const secondInput = calculator.querySelector('input[type="number"]:nth-of-type(2)');
-const resultInput = calculator.querySelector('input[type="number"]:nth-of-type(3)');
-const buttons = calculator.querySelectorAll('button');
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const firstValue = parseFloat(firstInput.value);
-    const secondValue = parseFloat(secondInput.value);
-    let result;
-    switch (button.textContent) {
-      case '+':
-        result = firstValue + secondValue;
-        break;
-      case '-':s
-        result = firstValue - secondValue;
-        break;
-      case 'X':
-        result = firstValue * secondValue;
-        break;
-      case '/':
-        result = firstValue / secondValue;
-        break;
-      case '^':
-        result = Math.pow(firstValue, secondValue);
-        break;
-      case '√¯':
-        result = Math.pow(firstValue, 1 / secondValue);
-        break;
-    }
-    resultInput.value = result;
-  });
-});
+const buttonSuma = document.getElementById("suma");
+const buttonResta = document.getElementById("resta");
+const buttonMultiplicacion = document.getElementById("multiplicacion");
+const buttonDivision = document.getElementById("division");
+const buttonPotencia = document.getElementById("potencia");
+const buttonRaiz = document.getElementById("raiz");
+
+function suma (){
+  input3.value = Number(input1.value)+Number(input2.value);
+}
+function resta (){
+  input3.value = Number(input1.value)-Number(input2.value);
+}
+function multiplicacion (){
+  input3.value = Number(input1.value)*Number(input2.value);
+}
+function division (){
+  if(b<0){
+    input3.value = Number(input1.value)/Number(input2.value);
+  }
+}
+function potencia (){
+  input3.value = Number(input1.value)**Number(input2.value);
+}
+function raiz (){
+  input3.value = Number(input1.value)**(1/Number(input2.value));
+}
+
+buttonSuma.addEventListener("click",suma)
+buttonResta.addEventListener("click",resta)
+buttonMultiplicacion.addEventListener("click",multiplicacion)
+buttonDivision.addEventListener("click",division)
+buttonPotencia.addEventListener("click",potencia)
+buttonRaiz.addEventListener("click",raiz)
