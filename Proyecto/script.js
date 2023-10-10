230b7887513c779afdae63f82438a269281f8b48
Santiago Nieto <Santiagonm2002@gmail.com>
2023-10-10 16:31:52 -0500

    add edit person

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -1,11 +1,40 @@
 let listaPersonas = [];
 
 const Button = document.getElementById("personButton");
+const ButtonE = document.getElementById("editPersonButton");
 const Table = document.getElementById("personTable");
+const Hidden = document.getElementsByClassName("hidden");
+const update = document.getElementById("update");
+const create = document.getElementById("create");
 
 Button.addEventListener("click",addPerson);
+ButtonE.addEventListener("click",editPerson);
+update.addEventListener("click",removeHidden);
+create.addEventListener("click",addHidden);
 
+function chargePerson(index){
+    console.log(index);
+    document.getElementById("codeI").value=listaPersonas[index].code;
+    document.getElementById("name").value=listaPersonas[index].name;
+    document.getElementById("lastName").value=listaPersonas[index].lastName;
+    document.getElementById("email").value=listaPersonas[index].email;
+    document.getElementById("numberPhone").value=listaPersonas[index].numberPhone;
+    document.getElementById("image").value=listaPersonas[index].image;
+}
+function addHidden(){
+    ButtonE.classList.add("hidden");
+    Button.classList.remove("hidden");
+    document.getElementById("codeL").classList.add("hidden");
+    document.getElementById("codeI").classList.add("hidden");
+}
+function removeHidden(){
+    document.getElementById("codeL").classList.remove("hidden");
+    document.getElementById("codeI").classList.remove("hidden");
+    ButtonE.classList.remove("hidden");
+    Button.classList.add("hidden");
+}
 function Person(name,lastName,email,numberPhone,image){
+    this.code=listaPersonas.length;
     this.name=name;
     this.lastName=lastName;
     this.email=email;
@@ -24,7 +53,7 @@ function addPerson(){
     printPerson(tPerson);    
 }
 function printPerson(person){
-    personValues = Object.values(person);  
+    const personValues = Object.values(person);  
     let row = document.createElement("tr");
     for (let i = 0; i < personValues.length-1; i++) {
         let text = document.createElement("th");
@@ -36,5 +65,34 @@ function printPerson(person){
         img.src = personValues.at(-1);
     text.appendChild(img);
     row.appendChild(text);
+    row.addEventListener("click",()=>{
+        removeHidden();
+        chargePerson(personValues[0]);
+    });
     Table.appendChild(row);
+}
+function editPerson(){ 
+    for(let i=0;i<listaPersonas.length;i++){
+        if(document.getElementById("codeI").value == listaPersonas[i].code){
+            listaPersonas[i].name = document.getElementById("name").value;
+            listaPersonas[i].lastName = document.getElementById("lastName").value;
+            listaPersonas[i].email = document.getElementById("email").value;
+            listaPersonas[i].numberPhone = document.getElementById("numberPhone").value;
+            listaPersonas[i].image = document.getElementById("image").value;
+        }
+    }
+    updateTable();
+}
+function updateTable(){
+    Table.innerHTML=`<tr>
+    <th>Code</th>
+    <th>Name</th>
+    <th>LastName</th>
+    <th>Email</th>
+    <th>numberPhone</th>
+    <th>image</th>
+    </tr>`;
+    for(i of listaPersonas){
+        printPerson(i);
+    }
 }
